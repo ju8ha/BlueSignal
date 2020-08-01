015198e517cf82d99e4d9653a713e0d2b435f9eb
ju8ha <mukpobu@gmail.com>
2020-08-01 22:46:33 +0900

    proto host

---
+++ application/host/app/src/main/java/com/example/bluesignal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bluesignal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //홈 화면 엑티비티
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
