5d092ff936eee1cfefb6f53f86767e0ed40fb976
baekyeseul <201802103@o.cnu.ac.kr>
2020-08-15 17:02:42 +0900

    [Guest]방문내역,문진표

---
+++ application/guest/app/src/main/java/com/example/bluesignal/VisitData.java
@@ -0,0 +1,30 @@
+package com.example.bluesignal;
+
+public class VisitData {
+    private String title;
+    private String content;
+    private String state;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state= state;
+    }
+}
